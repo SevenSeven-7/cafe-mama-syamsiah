7d70126fce7e2c4af2f93dc95701b192ede97b4d
SevenSeven-7 <senimansetia2@gmail.com>
2026-01-03 22:41:20 +0700

    Cafe Mama Syamsiah

--- JS/script_daricafe.js
+++ JS/script_daricafe.js
@@ -1,13 +1,10 @@
-// TOGGLE CLASS ACTIVE UNTUK HAMBURGER MENU //
 const navbarNav = document.querySelector('.navbar-nav');
 
-// KETIKA HAMBURGER MENU DI KLIK //
 document.querySelector('#hamburger-menu').onclick = (e) => {
   e.preventDefault();
   navbarNav.classList.toggle('active');
 };
 
-// KLIK DI LUAR SIDEBAR UNTUK MENGHILANGKAN NAV //
 const hamburger = document.querySelector('#hamburger-menu');
 
 document.addEventListener('click', function(e) {
